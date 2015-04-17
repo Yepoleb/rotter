aa6ef9c037d7adfc3a5988e8ac8b1f62d3a94981
vladdeSV <v@vladde.net>
2015-04-17 23:16:45 +0200

    Initial commit

---
+++ source/app.d
@@ -0,0 +1,31 @@
+import std.stdio;
+
+char[] input = "This is a ROT. Guvf vf n EBG".dup;
+immutable static string[2] check = ["abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+
+size_t amountOfRotation = 13;
+
+void main() {
+	ROTate(input, amountOfRotation);
+}
+
+void ROTate(char[] s, int amountToRotate) {
+	foreach(n; 0 .. s.length){
+		s[n] = rotateCharUniversal(s[n], amountToRotate);
+	}
+
+	writeln(input);
+}
+
+char rotateCharUniversal(char charToRotate, int amount) {
+	foreach(a; 0 .. check[0].length){
+		if(charToRotate == check[0][a]){
+			return check[0][(a + amount) % check[0].length];
+		}
+		if(charToRotate == check[1][a]){
+			return check[1][(a + amount) % check[1].length];
+		}
+	}
+
+	return charToRotate;
+}
