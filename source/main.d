1145acc7baa8daec71d08cd800dc472b30ce542d
vladdeSV <v@vladde.net>
2015-04-21 07:12:21 +0200

    Added comments
    Also changed around a bit with the code.

--- source/main.d
+++ source/main.d
@@ -1,34 +1,42 @@
 import std.stdio;
 
+size_t alphabetLenght;
+immutable static string[2] check = ["abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ"]; // Qucik method for me to have both big and small letters.
+
+static this(){
+
+	assert(check[0].length == check[1].length); //Make sure both alphabets are the same lenght. Just to make sure :)
+
+//	foreach(x; 0 .. check.length) //Foreach function to check all "versions" of the alphabets are the same length. I loop through them all incase you'd want more "version" of an alphabeth.
+//		assert(check[0].length == check[x].length);
+
+	alphabetLenght = check[0].length; //Sets the lenght to the alphabets lenghts.
+}
+
 // PART YOU EDIT ///////////////////////////////////////
 
-char[] input = "This is a ROT. Guvf vf n EBG".dup;
-size_t amountOfRotation = 13;
+string input = "This is a ROT. Guvf vf n EBG"; // String to rotate.
+size_t amountOfRotation = 39; // How far you want to rotate the above string.
 
 ////////////////////////////////////////////////////////
 
-immutable static string[2] check = ["abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-
-void main() {
-	ROTate(input, amountOfRotation);
-}
-
-
-void ROTate(char[] s, int amountToRotate) {
-	foreach(n; 0 .. s.length){
-		s[n] = rotateCharUniversal(s[n], amountToRotate);
+string ROTate(string str, int amountToRotate) {
+	char[] mutableString = str.dup; // Create a mutable version of the string.
+	foreach(n; 0 .. mutableString.length){ // For each character in that mutable char array...
+		mutableString[n] = rotateCharUniversal(mutableString[n], amountToRotate); // Do the rotation magic.
 	}
-
-	writeln(input);
+	return mutableString.idup; // Return as an immutable string.
 }
 
 char rotateCharUniversal(char charToRotate, int amount) {
-	foreach(n; 0 .. check.length)
-		foreach(a; 0 .. check[n].length){
-			if(charToRotate == check[n][a]){
-				return check[n][(a + amount) % check[n].length];
-			}
-		}
+	foreach(n; 0 .. check.length) // For every thing in check[]...
+		// And for whatever is in that string array, loop through the string inside.
+		foreach(a; 0 .. alphabetLenght) /* This loop should be "check[n].length" instead of 26, but everything in there should be 26 characters anyways */ 
+			if(charToRotate == check[n][a]) // Finds the location of the character entered.
+				return check[n][(a + amount) % check[n].length]; // Then at last return the character at the correct offset (rotated forward).
+	return charToRotate; // If the character isn't found (any character that isn't in the alphabeth, such as a space), return the original character.
+}
 
-	return charToRotate;
+void main() {
+	writeln(ROTate(input, amountOfRotation));
 }
